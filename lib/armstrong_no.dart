0fd983c09b69f7e271855ddc641bdee09acc6042
Rabbaani <gulrabbaani@gmail.com>
2023-12-13 23:15:03 +1200

    File Modified

--- lib/armstrong_no.dart
+++ lib/armstrong_no.dart
@@ -1,17 +1,79 @@
 import 'dart:io';
+import 'dart:math';
 
 void main(){
 
-  stdout.write("Enter any number to check if it is a Armstrong number");
+  stdout.write("Enter any number to check if it is a Armstrong number: ");
 
   int no = int.parse(stdin.readLineSync().toString());
 
   //length
   int length = 0;
+  int tempNo = no;
 
-  while(no > 0){
-    no = no~/10;
-    length += 1;
+  while(tempNo > 0){
+    tempNo = tempNo~/10;
+    length ++;
   }
 
+  //result
+  tempNo = no;
+
+  num result = 0;
+
+  while(tempNo > 0){
+    int remainder = tempNo % 10;
+    result = result + pow(remainder, length);
+    tempNo = tempNo~/10;
+  }
+
+  if(no == result){
+    print("It is an armstrong number.");
+  } else {
+    print("It is not an armstrong number.");
+  }
+
+
 }
+
+///How many armstrong number the user wants
+/*
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  stdout.write("Enter how many Armstrong numbers you want to find: ");
+  int count = int.parse(stdin.readLineSync()!);
+
+  int number = 1;
+
+  while (count > 0) {
+    if (isArmstrongNumber(number)) {
+      print("$number is an Armstrong number.");
+      count--;
+    }
+    number++;
+  }
+}
+
+bool isArmstrongNumber(int no) {
+  int length = 0;
+  int tempNo = no;
+
+  while (tempNo > 0) {
+    tempNo = tempNo ~/ 10;
+    length++;
+  }
+
+  tempNo = no;
+  num result = 0;
+
+  while (tempNo > 0) {
+    int remainder = tempNo % 10;
+    result = result + pow(remainder, length);
+    tempNo = tempNo ~/ 10;
+  }
+
+  return no == result;
+}
+*/
